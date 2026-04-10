40801871592b472a7ffb259cca518fb5e4cd7533
argaardana1 <remajasehat007@gmail.com>
2026-04-10 07:06:05 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,19 @@
-import "./globals.css"; // Baris ini jangan sampai hilang!
-import { Inter } from "next/font/google";
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700', '900'] });
+
+export const metadata = {
+  title: 'MixPay Premium Gateway',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#0F172A] selection:bg-blue-500 selection:text-white`}>
+        {children}
+      </body>
     </html>
   );
 }
