7b88b4376cec8459dd216ae3593d7d1190882d4a
argaardana1 <remajasehat007@gmail.com>
2026-04-10 07:14:07 +0700

    Update and rename not-found to not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -4,6 +4,8 @@ import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
   const router = useRouter();
-  useEffect(() => { router.replace('/'); }, []);
+  useEffect(() => {
+    router.replace('/'); // Paksa kembali ke halaman utama jika URL ngawur
+  }, [router]);
   return null;
 }
